e234493f2df3d8c6b1bae5e220364944effa0af1
reinaldo <reinaldo58972@gmail.com>
2024-02-16 19:54:06 -0300

    enviando projeto para deploy

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import WellDone from "../img/welldone.svg";
+
+import './GameOver.css'
+
+const GameOver = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+    return (
+        <div id="gameover">
+            <h2>Finalizado</h2>
+            <p>Nota: {quizState.score}</p>
+            <p>Você acertou  {quizState.score} de  {quizState.questions.length}{" "} perguntas.</p>
+            <img src={WellDone} alt="Fim do Quiz" />
+            <button onClick={() => dispatch({ type: "NEW_GAME" })}>Reiniciar</button>
+        </div>
+    )
+}
+
+export default GameOver
